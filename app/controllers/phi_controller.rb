4afa0123a1c5c614799545df446e3bc87b009788
Henry Netzer <hnetzer19@gmail.com>
2018-08-17 15:29:12 -0700

    ajax call to get lab results

--- app/controllers/phi_controller.rb
+++ app/controllers/phi_controller.rb
@@ -10,4 +10,18 @@ class PhiController < ApplicationController
 
     render json: JSON.parse(response.body)
   end
+
+  def lab_results
+    access_token = params[:access_token]
+    patient_id = params[:patient_id]
+
+    auth = "Bearer " + params[:access_token]
+    http = HTTP.headers("Authorization" => auth)
+    response = http.get("https://drchrono.com/api/lab_results?patient=" + patient_id)
+
+    puts "response from drchrono lab_results endpoint"
+    puts response.status
+
+    render json: JSON.parse(response.body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'redirect' => 'redirect#index'
 
   get 'phi/patients' => 'phi#patients'
+
+  get 'phi/lab_results' => 'phi#lab_results'
 end
